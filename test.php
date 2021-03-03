0f7c7c237f6e4a5a8867350c4346c5805c021fca
immunoHLA <78468896+immunoHLA@users.noreply.github.com>
2021-03-03 11:05:16 +0100

    Update test.php

--- test.php
+++ test.php
@@ -4,14 +4,8 @@ require_once "../redcap_connect.php";
 
 ## Your custom PHP code goes here. You may use any constants/variables listed in redcap_info().
 
-// OPTIONAL: Display the project header
-require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
-
 ## Your HTML page content goes here
 <html>
   <h1>Hello World</h1>
 </html>
-
-// OPTIONAL: Display the project footer
-require_once APP_PATH_DOCROOT . 'ProjectGeneral/footer.php';
 ?>
