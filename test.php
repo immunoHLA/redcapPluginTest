58975d73913d5c957f06b316d3891a623cbd6b7a
immunoHLA <78468896+immunoHLA@users.noreply.github.com>
2021-03-03 11:13:40 +0100

    Update test.php

--- test.php
+++ test.php
@@ -3,6 +3,7 @@
 ## Your custom PHP code goes here. You may use any constants/variables listed in redcap_info().
 
 ## Your HTML page content goes here
+<!DOCTYPE html>
 <html>
   <h1>Hello World</h1>
 </html>
