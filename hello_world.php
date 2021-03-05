1e36ca4794fa944e4c17173c1a119c4f5a4c2758
immunoHLA <78468896+immunoHLA@users.noreply.github.com>
2021-03-05 13:42:16 +0100

    Update hello_world.php
    Ajout de l'appel au fichier de connexion à REDCap

--- hello_world.php
+++ hello_world.php
@@ -1,6 +1,12 @@
 <?php
+// Disable REDCap's authentication
+define("NOAUTH", true);
+
+// Call the REDCap Connect file in the main "redcap" directory
+require_once"/var/www/html/redcap/redcap_connect.php";
+
+## Your custom PHP code goes here.
+
 Print "Hello,World!";
-?>
-<?php
 Echo "Hello, World!";
 ?>
