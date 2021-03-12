e12395eff69b74d8bfe7ffa57e46e51ebc10d5f8
immunoHLA <78468896+immunoHLA@users.noreply.github.com>
2021-03-12 11:35:11 +0100

    Update test.py
    Modification of the SQL Request.

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ driver= '{ODBC Driver 17 for SQL Server}'
 
 with pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
     with conn.cursor() as cursor:
-        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+        cursor.execute("SELECT PatientID, LastName, FirstName, FORMAT(Dob, 'dd/MM/yyyy', 'fr-fr') AS Dob FROM Patient WHERE PatientID like '%$Identity%' or FamilyID like '%$Identity%' or LastName like '%$Identity%' or FirstName like '%$Identity%' and IsActive = 1")
         row = cursor.fetchone()
         while row:
             print (str(row[0]) + " " + str(row[1]))
